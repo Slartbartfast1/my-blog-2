3afa600f9f1c4f259b310cbfade0e0eb77a05735
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-21 23:41:31 +0800

    Update src/components/ArticleBox.vue

--- src/components/ArticleBox.vue
+++ src/components/ArticleBox.vue
@@ -1,13 +1,21 @@
 <template>
+    <Card dis-hover="true">
+        <Avatar src="https://slartbartfast.cn/admin/avatar/avatar%20(1)_gaitubao_com_296x296.png" size="large"/>
+        <div class="imgTitle"><img
+                src="https://slartbartfast.cn/static/assets/img/ES6%E4%B9%8Blet%E5%92%8Cconstwallhaven-39700_gaitubao_com_1135x480.jpg"
+                alt="标题图片"></div>
+        <div class="title">
+            <p>Here is title???</p>
+        </div>
+        <div class="summary">
+            <i>summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary summarysummarysummarysummary
+                summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary</i>
 
-
-            <Card dis-hover="true">
-                <Avatar src="https://slartbartfast.cn/admin/avatar/avatar%20(1)_gaitubao_com_296x296.png" size="large"/>
-                <div class="imgTitle"><img
-                        src="https://slartbartfast.cn/static/assets/img/ES6%E4%B9%8Blet%E5%92%8Cconstwallhaven-39700_gaitubao_com_1135x480.jpg"
-                        alt="标题图片"></div>
-                <div class="title"><p>Here is title</p></div>
-</Card>
+        </div>
+        <div class="boxBottom">
+            <Icon type="ios-thumbs-up-outline" size="30"/>
+        </div>
+    </Card>
 
 
 </template>
@@ -20,21 +28,42 @@
 
 <style scoped lang="scss">
 
-    .ivu-avatar{
-        height:64px;
-        width:64px;
-        border-radius: 32px;
+
+    .ivu-avatar {
+        height: 50px;
+        width:50px;
+        border-radius: 25px;
+
     }
-    .imgTitle  {
-        padding:20px 0 15px 0;
-       img{
-           height: 250px;
-           width: 100%;}
+
+    .imgTitle {
+        margin-top:10px;
+        img {
+            height: 200px;
+            width: 100%;
+        }
     }
-    .title{
-        padding-left: 10px;
-        color:black;
+
+    .title {
+        color: black;
         font-size: 30px;
         font-weight: 600;
+    }
+    .summary{
+        ::before{
+            content: '';
+            position: absolute;
+            display: block;
+            height:100%;
+            width: 100%;
+            background-image: linear-gradient(to bottom,rgba(255,255,255,0) 0,rgba(255,255,255,.7) 100%);
+        }
+        position:relative;
+        word-break: break-word;
+        color:rgba(0,0,0,.7);
+        font-size:16px;
+    }
+    .boxBottom{
+        padding-top:10px;
     }
 </style>
