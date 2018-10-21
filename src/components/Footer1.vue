d01be8f6077aa0bca281d40429d7dfec7ae0a16c
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-21 22:59:09 +0800

    Update src/components/Footer1.vue

---
+++ src/components/Footer1.vue
@@ -0,0 +1,20 @@
+<template>
+    <Footer>
+        <div>
+        </div>
+    </Footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer1"
+    }
+</script>
+
+<style scoped lang="scss">
+.ivu-layout-footer div{
+    height:60px;
+    width:100%;
+    color: #ffffff;
+}
+</style>
