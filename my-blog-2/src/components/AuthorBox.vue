6b36efe643e1f88363953031d26d3cb346660fbe
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-21 22:19:23 +0800

    Update my-blog-2/src/components/AuthorBox.vue

---
+++ my-blog-2/src/components/AuthorBox.vue
@@ -0,0 +1,21 @@
+<template>
+
+            <div class="author">
+            </div>
+
+</template>
+
+<script>
+    export default {
+        name: "AuthorBox"
+    }
+</script>
+
+<style scoped lang="scss">
+.author{
+    height: 300px;
+    background-color:#FFF;
+    border:1px solid rgba(0,0,0,.1);
+    margin-top:30px;
+}
+</style>
