bd73ce5ba69cab395fdaedad448c200f8616561f
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-22 14:54:13 +0800

    Update src/components/AuthorBox.vue

--- src/components/AuthorBox.vue
+++ src/components/AuthorBox.vue
@@ -1,8 +1,9 @@
 <template>
-
             <div class="author">
+                <div class="border">
+                </div>
+                <div class="avatar"><img src="https://slartbartfast.cn/admin/avatar/avatar%20(1)_gaitubao_com_296x296.png" alt=""></div>
             </div>
-
 </template>
 
 <script>
@@ -15,8 +16,23 @@
 .author{
     height: 300px;
     background-color:#FFF;
-    border:1px solid rgba(0,0,0,.1);
     margin-top:30px;
+    position: relative;
+
+    .avatar{
+        margin:0 auto;
+        width:160px;
+        height:160px;
+        border-radius: 80px;
+        border:1px dotted green;
+        overflow: hidden;
+        margin-top:96px;
+        /*position: absolute;*/
+        img{
+            width:100%;
+            height:100%;
+        }
+    }
     
 }
 </style>
