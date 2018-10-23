7f4f163e7c02df38baf2f44e8b993a787fdb3fff
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-24 00:10:54 +0800

    Update src/Index.vue

--- src/Index.vue
+++ src/Index.vue
@@ -1,54 +1,66 @@
 <template>
     <div id="app">
+        <Layout>
+<Header>
         <HeadNav></HeadNav>
+</Header>
+        <Content>
         <Row type="flex" justify="center">
             <Col span="15" >
                 <AuthorBox></AuthorBox>
             </Col>
-
         </Row>
-
-
         <Row type="flex" justify="center" class="featured">
             <Col span="15">
                 <div class="cate"><b>Featured</b></div>
-                <ArticleBox></ArticleBox>
+                <Featured></Featured>
             </Col>
         </Row>
         <Row type="flex" justify="center" class="featured">
             <Col span="15">
-                <div class="cate"><b>Lasted</b></div>
-                <ArticleBox></ArticleBox>
+                <div class="cate"><b>Latest</b></div>
+                <Latest></Latest>
             </Col>
         </Row>
-
+        </Content>
+        <Footer>
         <Footer1></Footer1>
-
+        </Footer>
+        </Layout>
     </div>
 </template>
 <script>
     import HelloWorld from './components/HelloWorld.vue'
     import HeadNav from './components/HeadNav.vue'
     import Footer1 from './components/Footer1.vue'
-    import ArticleBox from './components/ArticleBox.vue'
+    import Featured from './components/Featured.vue'
+    import Latest from './components/Latest.vue'
     import AuthorBox from './components/AuthorBox'
 
 
     export default {
+
         name: 'app',
         components: {
             HelloWorld,
             HeadNav,
             Footer1,
-            ArticleBox,
             AuthorBox,
-        }
+            Featured,
+            Latest,
+        },
     }
 </script>
 
 <style lang="scss">
     #app {
-
+        .ivu-layout-header{
+            background-color: #ddd;
+            padding:0;
+        }
+        .ivu-layout-content{
+            background-color: #fff;
+        }
         .cate{
             color:black;
             padding:40px 0;
