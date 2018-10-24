b5d035895cc810bf1629820759cab14f5ab081e3
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-24 16:22:51 +0800

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import Index from './Index.vue'
 import router from './router'
 import store from './store'
 import './plugins/iview.js'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
