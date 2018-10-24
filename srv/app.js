984098c780b64da0ddae461da69d2fd54e861ce7
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-24 22:04:32 +0800

    API

--- srv/app.js
+++ srv/app.js
@@ -27,36 +27,36 @@
 // }
 const express = require('express');
 const app = express();
-const $sql=require('./API/sql')
+const $sql = require('./API/sql')
 
 // 跨域
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
 //精选
-app.get('/featured',(req,res)=>{
-    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=1 and articleid!=228`, (vals)=>{
-        let rows = JSON.stringify(vals);
+app.get('/featured', (req, res) => {
+    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=1 and articleid!=228`, (data) => {
+        let rows = JSON.stringify(data);
         res.send(rows)
     })
 })
 
 //时间排序
-app.get('/latest',(req,res)=>{
-    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc`, (vals)=>{
-        let rows = JSON.stringify(vals);
+app.get('/latest', (req, res) => {
+    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc`, (data) => {
+        let rows = JSON.stringify(data);
         res.send(rows)
     })
 });
 
 
-app.listen(8001,() => {
+app.listen(8001, () => {
     console.log('连接成功');
 })
 
