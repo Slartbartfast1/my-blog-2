4d7ec3f295a3c9d1998bc4b2666193733fe73637
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-26 17:26:44 +0800

    Update src/views/Tag.vue

---
+++ src/views/Tag.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <Row type="flex" justify="center" class="featured">
+            <Col span="12">
+                <Affix v-show="this.show">
+                    <div class="cate">
+                        <a href="#">
+                            <Icon type="md-pricetag"/>
+                        </a>
+                        <b>{{this.$route.query.name}}</b>
+                    </div>
+                </Affix>
+                <TagComponent></TagComponent>
+            </Col>
+        </Row>
+
+    </div>
+</template>
+
+<script>
+    import TagComponent from '../components/TagComponent'
+
+    export default {
+        name: 'tag',
+        components: {
+    TagComponent,
+        },
+        data() {
+            return {
+                show: true
+            }
+        },
+        methods: {
+            mission: function () {
+                return this.show = !this.show
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .cate {
+        color: black;
+        font-size: 25px;
+        font-weight: 400;
+    }
+</style>
