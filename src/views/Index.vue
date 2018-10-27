b34969438b91e9430fc616c976507dcf0b868273
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-28 02:18:28 +0800

    Update src/views/Index.vue

---
+++ src/views/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <Content>
+        <Row type="flex" justify="center">
+            <Col span="15">
+                <AuthorBox></AuthorBox>
+            </Col>
+        </Row>
+        <router-view/>
+    </Content>
+</template>
+
+<script>
+    import AuthorBox from '../components/Featured'
+    import Featured from '../components/Featured'
+    import Latest from '../components/Latest'
+    export default {
+        name: "Index",
+        component:
+        AuthorBox,
+    }
+</script>
+
+<style scoped>
+
+</style>
