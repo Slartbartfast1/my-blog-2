facd59818f556a1a94c4d8f35b45f6d699391fb4
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-28 02:18:46 +0800

    Update src/Index.vue

--- src/Index.vue
+++ src/Index.vue
@@ -1,20 +1,20 @@
 <template>
     <div id="app">
         <Layout>
-<Header>
-        <HeadNav></HeadNav>
-</Header>
-        <Content>
-        <Row type="flex" justify="center" id="hero">
-            <Col span="15" >
-                <AuthorBox></AuthorBox>
-            </Col>
-        </Row>
-        <router-view/>
-        </Content>
-        <Footer>
-        <Footer1></Footer1>
-        </Footer>
+            <Header>
+                <HeadNav></HeadNav>
+            </Header>
+            <Content>
+                <Row type="flex" justify="center">
+                    <Col span="15">
+                        <AuthorBox></AuthorBox>
+                    </Col>
+                </Row>
+                <router-view/>
+            </Content>
+            <Footer>
+                <Footer1></Footer1>
+            </Footer>
         </Layout>
     </div>
 </template>
@@ -24,7 +24,6 @@
     import AuthorBox from './components/AuthorBox'
 
     export default {
-
         name: 'app',
         components: {
             HeadNav,
@@ -36,31 +35,34 @@
 
 <style lang="scss">
     #app {
-        .highlight{
-            &:after{
-                display:block;
+        .highlight {
+            &:after {
+                display: block;
                 content: "";
                 height: 3px;
                 width: 0px;
                 background-color: #0082a7;
                 transition: width .5s ease;
             }
-            &:hover{
-                &:after{
-                    width:100%;
+            &:hover {
+                &:after {
+                    width: 100%;
                 }
 
             }
         }
+        #hero {
+            /*<!--background:no-repeat 0 -600px  url(https://cdn.vuetifyjs.com/images/parallax/material2.jpg);-->*/
+        }
 
-        .ivu-card{
-            margin:48px 0 ;
+        .ivu-card {
+            margin: 48px 0;
         }
-        .ivu-layout-header{
+        .ivu-layout-header {
             background-color: #ddd;
-            padding:0;
+            padding: 0;
         }
-        .ivu-layout-content{
+        .ivu-layout-content {
             background-color: #fff;
         }
 
