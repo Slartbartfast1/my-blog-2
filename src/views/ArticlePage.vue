633092440111f620c5ff3cd12479bb987e43ce02
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-28 02:18:31 +0800

    Update src/views/ArticlePage.vue

---
+++ src/views/ArticlePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <div class="imgtitle">
+            <div class="title">
+                泛银河系含漱爆破液
+            </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ArticlePAge"
+    }
+</script>
+
+<style scoped>
+.imgtitle{
+    height:500px;
+    width:100%;
+    background:no-repeat url(https://cdn.vuetifyjs.com/images/parallax/material2.jpg);
+}
+</style>
