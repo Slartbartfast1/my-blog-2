4813b060c490a07742f7cafade2639a8af04c138
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-29 15:44:58 +0800

    更新查询语句

--- srv/app.js
+++ srv/app.js
@@ -15,7 +15,7 @@ app.all('*', function (req, res, next) {
 
 //精选
 app.get('/featured', (req, res) => {
-    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=1 and articleid!=228 `, (data) => {
+    $sql.mySelect(`select articleid,title,gist,createTime,author,imgurl,category,view from article where top=1 and articleid!=228 `, (data) => {
         let rows = JSON.stringify(data);
         res.send(rows)
     })
@@ -23,26 +23,51 @@ app.get('/featured', (req, res) => {
 
 //时间排序
 app.get('/latest', (req, res) => {
-    let params=+req.query.load||4;
-    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc limit 0,${params} `, (data) => {
+    let params = +req.query.load || 4;
+    $sql.mySelect(`select articleid,title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc limit 0,${params} `, (data) => {
         let rows = JSON.stringify(data);
         res.send(rows)
     })
 });
 
 //获得标签名称
-app.get('/tags/name',(req,res)=>{
-    $sql.mySelect(`select * from categories`,(data)=>{
-        let rows=JSON.stringify(data);
+app.get('/tags/name', (req, res) => {
+    $sql.mySelect(`select * from categories`, (data) => {
+        let rows = JSON.stringify(data);
         res.send(rows)
     })
 })
 
 //获得文章列表
-app.get('/tags/article',(req,res)=>{
-    let id=req.query.tag;
-    $sql.mySelect(`select title,gist,createTime,author,imgurl,category,view from article where articleid!=228 and category=`+id,(data)=>{
-        let rows=JSON.stringify(data);
+app.get('/tags/article', (req, res) => {
+    let id = req.query.tag;
+    $sql.mySelect(`select * from article where articleid!=228 and category=${id}`, (data) => {
+        let rows = JSON.stringify(data);
+        res.send(rows)
+    })
+});
+
+//获得文章内容
+app.get('/article', (req, res) => {
+    let id = req.query.id;
+    $sql.mySelect(`select 
+    article.articleid,
+    article.title,
+    article.author,
+    article.createTime,
+    article.content,
+    article.top,
+    article.view,
+    article.imgurl, 
+    article.gist,
+    article.category,
+    categories.id,
+    categories.categories as categoryName
+    from article inner join categories 
+    on article.category=categories.id 
+    where article.articleid=${id}`
+        ,(data) => {
+        let rows = JSON.stringify(data);
         res.send(rows)
     })
 })
