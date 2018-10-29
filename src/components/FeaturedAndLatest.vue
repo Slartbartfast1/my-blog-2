e1a1090e5950c4fa67557bb58a625c0b2548c115
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-29 15:45:33 +0800

    Update src/components/FeaturedAndLatest.vue

---
+++ src/components/FeaturedAndLatest.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <Row type="flex" justify="center" class="featured">
+            <Col span="12">
+                <Affix v-show="this.show">
+                    <div class="cate">
+                        <a href="#">
+                            <Icon type="md-pricetag"/>
+                        </a>
+                        <b>Featured</b>
+                    </div>
+                </Affix>
+                <Featured></Featured>
+            </Col>
+        </Row>
+        <Row type="flex" justify="center" class="featured">
+            <Col span="12">
+                <Affix @on-change="mission">
+                    <div class="cate">
+                        <a href="#">
+                            <Icon type="md-pricetag"/>
+                        </a>
+                        <b>Latest</b>
+                    </div>
+                </Affix>
+                <Latest></Latest>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+    import Featured from './Featured'
+    import Latest from './Latest'
+
+    export default {
+        name: 'home',
+        components: {
+            Featured,
+            Latest,
+        },
+        data() {
+            return {
+                show: true
+            }
+        },
+        methods: {
+            mission: function () {
+                return this.show = !this.show
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .cate {
+        color: black;
+        font-size: 25px;
+        font-weight: 400;
+    }
+</style>
