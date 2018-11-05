86bce1c4bc79a0384585ef77b37fa63ff9f074ea
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-05 17:14:07 +0800

    Update src/views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,13 @@
 <template>
-    <div>
-        <Row type="flex" justify="center" class="featured">
-            <Col span="12">
-                <Affix v-show="this.show">
-                    <div class="cate">
-                        <a href="#">
-                            <Icon type="md-pricetag"/>
-                        </a>
-                        <b>Featured</b>
-                    </div>
-                </Affix>
-                <Featured></Featured>
-            </Col>
-        </Row>
-        <Row type="flex" justify="center" class="featured">
-            <Col span="12">
-                <Affix @on-change="mission">
-                    <div class="cate">
-                        <a href="#">
-                            <Icon type="md-pricetag"/>
-                        </a>
-                        <b>Latest</b>
-                    </div>
-                </Affix>
-                <Latest></Latest>
-            </Col>
-        </Row>
-    </div>
+  <HelloWorld />
 </template>
 
 <script>
-    import Featured from '../components/Featured'
-    import Latest from '../components/Latest'
+  import HelloWorld from '../components/HelloWorld'
 
-    export default {
-        name: 'home',
-        components: {
-            Featured,
-            Latest,
-        },
-        data() {
-            return {
-                show: true
-            }
-        },
-        methods: {
-            mission: function () {
-                return this.show = !this.show
-            }
-        }
+  export default {
+    components: {
+      HelloWorld
     }
+  }
 </script>
-<style scoped lang="scss">
-    .cate {
-        color: black;
-        font-size: 25px;
-        font-weight: 400;
-    }
-</style>
