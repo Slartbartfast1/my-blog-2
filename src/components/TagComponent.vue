04c152ce29a07c718dc4d736ee4db2bfe93b7f26
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-05 19:50:45 +0800

    watch:{"$route": "getArticleByTag", }

--- src/components/TagComponent.vue
+++ src/components/TagComponent.vue
@@ -1,51 +1,53 @@
 <template>
-    <div class="tags">
-        <Card v-for="(item,index) in tags" :key="item.slug">
-            <Avatar src="https://slartbartfast.cn/admin/avatar/avatar%20(1)_gaitubao_com_296x296.png" size="small"/>
-            <div class="info">
-                <span class="author"><div>{{item.author}}</div></span>
-                <i class="date">
-                    <div>{{item.createTime}}</div>
-                </i>
-            </div>
-            <Icon type="ios-star" size="20"/>
-            <div class="imgTitle" v-html="html[index]"  v-src="'http://58.87.107.26/'+item.imgurl">{{html[index]}}</div>
-            <div class="title">
-                <router-link :to="{path:'/article',query:{id:item.articleid}}">
-                <p>{{item.title}}</p>
-                    </router-link>
-            </div>
-            <div class="summary">
-                <i>{{item.gist}}</i>
-            </div>
-            <div class="boxBottom">
-                <Icon type="ios-thumbs-up-outline" size="26"/>
-                <span class="good"> 1111</span>
-                <div class="comment"><span>{{item.view}} responses </span>
-                    <Icon type="ios-text-outline" size="26"/>
+    <keep-alive>
+        <div class="tags">
+            <Card v-for="(item,index) in tags" :key="item.slug">
+                <Avatar src="https://slartbartfast.cn/admin/avatar/avatar%20(1)_gaitubao_com_296x296.png" size="small"/>
+                <div class="info">
+                    <span class="author"><div>{{item.author}}</div></span>
+                    <i class="date">
+                        <div>{{item.createTime}}</div>
+                    </i>
                 </div>
-            </div>
-        </Card>
+                <Icon type="ios-star" size="20"/>
+                <div class="imgTitle" v-html="html[index]" v-src="'http://58.87.107.26/'+item.imgurl">{{html[index]}}
+                </div>
+                <div class="title">
+                    <router-link :to="{path:'/article',query:{id:item.articleid}}">
+                        <p>{{item.title}}</p>
+                    </router-link>
+                </div>
+                <div class="summary">
+                    <i>{{item.gist}}</i>
+                </div>
+                <div class="boxBottom">
+                    <Icon type="ios-thumbs-up-outline" size="26"/>
+                    <span class="good"> 1111</span>
+                    <div class="comment"><span>{{item.view}} responses </span>
+                        <Icon type="ios-text-outline" size="26"/>
+                    </div>
+                </div>
+            </Card>
 
-    </div>
-
+        </div>
+    </keep-alive>
 </template>
 
 <script>
 
     export default {
-        name: "Featured",
+        name: "TagComponent",
         mounted: function () {
-            this.getArticleByTag()
-        },
-        updated: function () {
             this.getArticleByTag()
         },
         data() {
             return {
                 tags: [],
-                html:[],
+                html: [],
             }
+        },
+        watch: {
+            "$route": "getArticleByTag",//路由更新时执行方法
         },
         directives: {
             src: {
@@ -57,28 +59,60 @@
                     element.style.zIndex = '200'
                     element.style.transform = 'scale(1.1)'
                     element.src = binding.value
-                    element.style.filter='blur(20px)'
-                    element.style.transition='.3s ease all'
-                    element.onload=function(){
-                        element.style.filter='blur(0px)'
+                    element.style.filter = 'blur(20px)'
+                    element.style.transition = '.3s ease all'
+                    element.onload = function () {
+                        element.style.filter = 'blur(0px)'
                         element.style.transform = 'scale(1)'
                         let obj = el.querySelector('.img-thumb')
                         el.removeChild(obj)
                     };
                     el.appendChild(element)
                 },
+                componentUpdated: function (el, binding) {
+                    var element = new Image();
+                    element.className = "img-src"
+                    element.style.width = '100%'
+                    element.style.height = '200px'
+                    element.style.zIndex = '200'
+                    element.style.transform = 'scale(1.1)'
+                    element.src = binding.value
+                    element.style.filter = 'blur(20px)'
+                    element.style.transition = '.3s ease all'
+                    element.onload = function () {
+                        element.style.filter = 'blur(0px)'
+                        element.style.transform = 'scale(1)'
+                        let obj = el.querySelector('.img-thumb')
+                        el.removeChild(obj)
+                    };
+                    el.appendChild(element)
+                },//路由更新后
             }
         },
+
+        /*自定义指令生命周期
+    （1）bind： 只调用一次，指令第一次绑定到元素时调用，用这个钩子函数可以定义一个在绑定时执行一次的初始化动作。
+
+　　（2）inserted： 被绑定元素插入父节点时调用（父节点存在即可调用，不必存在于 document 中）。
+
+　　（3）update： 被绑定元素所在的模板更新时调用，而不论绑定值是否变化。通过比较更新前后的绑定值，可以忽略不必要的模板更新。
+
+　　（4）componentUpdated：被绑定元素所在模板完成一次更新周期时调用。
+
+　　（5）unbind：只调用一次， 指令与元素解绑时调用。*/
+
+
         methods: {
             getArticleByTag() {
                 this.$http.get(`http://localhost:8001/tags/article?tag=${this.$route.query.tag}`)
                     .then((res) => {
-                        this.tags = res.body
-                        this.html=[];
-                        this.tags.forEach((el)=>{
-                            let img=`<img src="http://58.87.107.26/${el.imgurl.slice(0, -4)}-thumb${el.imgurl.slice(-4)}" class="img-thumb" alt="标题图片" style="width:100%;height:100%">`
+                        this.tags = res.body;
+                        this.html = [];
+                        this.tags.forEach((el) => {
+                            var img = `<img src="http://58.87.107.26/${el.imgurl.slice(0, -4)}-thumb${el.imgurl.slice(-4)}" class="img-thumb" alt="标题图片" style="width:100%;height:100%">`
                             this.html.push(img)
                         })
+
                     })
             },
 
