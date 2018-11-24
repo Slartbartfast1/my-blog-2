851629ab61bf3ed3964cde0dd76dfadbcf6248ab
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-24 17:45:40 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
-import Index from './Index.vue'
+import Index from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/iview.js'
@@ -10,7 +9,7 @@ import  './plugins/highlight.js'
 import 'vuetify/dist/vuetify.min.css'
 import VueLazyLoad from 'vue-lazyload'
 
-Vue.use(VueLazyLoad)
+Vue.use(VueLazyLoad);
 
 Vue.use(VueResource);
 
