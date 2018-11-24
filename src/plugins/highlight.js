2aa820b25efa6d3dfbc2b410956c3acfbea3b1ea
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-24 17:45:24 +0800

    Update highlight.js

--- src/plugins/highlight.js
+++ src/plugins/highlight.js
@@ -4,10 +4,9 @@ import javascript from 'highlight.js/lib/languages/javascript.js'
 import 'highlight.js/styles/atom-one-light.css'
 Hljs.registerLanguage('javascript', javascript)
 let Highlight = {};
-Highlight.install = function (Vue, options) {
+Highlight.install = function (Vue) {
     Vue.directive('highlight', function (el) {
         let blocks = el.querySelectorAll('pre code');
-        console.log(blocks)
         blocks.forEach((block) => {
             Hljs.highlightBlock(block);
         })
