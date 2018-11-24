e1292f3ea53a5b18461add80a8bb9da44166d965
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-24 17:45:35 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Index from './views/Index'
 import ArticlePage from './views/ArticlePage'
 
@@ -32,42 +31,37 @@ const scrollBehavior = (to, from, savedPosition) => {
 
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-    scrollBehavior,
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index,
-        children:[{
-        path:'tag',
-            component:()=>import('./views/Tag.vue')
-        },
-            {
-                path:'tag',
-                component:()=>import('./views/Tag.vue')
-            },            {
-                path:'',
+    mode: 'history',
+    base: process.env.BASE_URL,scrollBehavior,
 
-                component:()=>import('./components/FeaturedAndLatest.vue'),
-
+    routes: [
+        {
+            path: '/',
+            name: 'Index',
+            component: Index,
+            children: [{
+                path: 'tag',
+                component: () => import('./views/Tag.vue')
             },
-        ]
-    },
-    // {
-    //   path: '/tag',
-    //   name: 'Tag',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/Tag.vue')
-    // },
-      {
-        path:'/article',
-          name:'article',
-          component:ArticlePage,
-          meta: { scrollToTop: true },
-      }
-  ]
+                 {
+                    path: '',
+                    component: () => import('./components/FeaturedAndLatest.vue'),
+                },
+            ]
+        },
+        // {
+        //   path: '/tag',
+        //   name: 'Tag',
+        //   // route level code-splitting
+        //   // this generates a separate chunk (about.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () => import(/* webpackChunkName: "about" */ './views/Tag.vue')
+        // },
+        {
+            path: '/article',
+            name: 'article',
+            component: ArticlePage,
+            meta: {scrollToTop: true},
+        }
+    ]
 })
