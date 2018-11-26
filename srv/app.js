6dc9e0f4c04b133b6edbe81078ade6736c2d5881
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-26 22:31:45 +0800

    Update app.js

--- srv/app.js
+++ srv/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const $sql = require('./API/sql')
+const $sql = require('./API/sql');
+const bodyParser=require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 跨域
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
@@ -21,23 +26,23 @@ app.get('/featured', (req, res) => {
     })
 })
 
-//时间排序
+//时间排序8
 app.get('/latest', (req, res) => {
-    if(req.query.total){
+    if (req.query.total) {
         $sql.mySelect(`select count(1) as num from article where top=2 and articleid!=228`, (data) => {
             let rows = JSON.stringify(data);
             res.send(rows)
         })
-    }else{
-        if(!req.query.start){
+    } else {
+        if (!req.query.start) {
             let params = +req.query.offset || 4;
             $sql.mySelect(`select articleid,title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc limit 0,${params} `, (data) => {
                 let rows = JSON.stringify(data);
                 res.send(rows)
             })
-        }else{
-            let start=req.query.start;
-            let size=req.query.size;
+        } else {
+            let start = req.query.start;
+            let size = req.query.size;
             $sql.mySelect(`select articleid,title,gist,createTime,author,imgurl,category,view from article where top=2 and articleid!=228 order by createTime desc limit ${start},${size} `, (data) => {
                 let rows = JSON.stringify(data);
                 res.send(rows)
@@ -65,8 +70,8 @@ app.get('/tags/article', (req, res) => {
     })
 });
 
-app.get('/random',(req,res)=>{
-    let limit=req.query.limit;
+app.get('/random', (req, res) => {
+    let limit = req.query.limit;
     $sql.mySelect(`select 
     articleid,
     author,
@@ -75,7 +80,7 @@ app.get('/random',(req,res)=>{
     createTime
     from article where articleid!=228 
     order by rand() limit ${limit}`
-        ,(data) => {
+        , (data) => {
             let rows = JSON.stringify(data);
             res.send(rows)
         })
@@ -100,12 +105,26 @@ app.get('/article', (req, res) => {
     from article inner join categories 
     on article.category=categories.id 
     where article.articleid=${id}`
-        ,(data) => {
-        let rows = JSON.stringify(data);
-        res.send(rows)
-    })
+        , (data) => {
+            let rows = JSON.stringify(data);
+            res.send(rows)
+        })
 });
 
+app.get('/comments', (req, res) => {
+    let articleid = req.query.articleid || 228;
+    $sql.mySelect(`select * from comments where articleid=${articleid}`
+        , data => {
+            let rows = JSON.stringify(data);
+            res.send(rows)
+        })
+});
+
+app.post('/comments', (req, res) => {
+    console.log(req.body);
+    $sql.myInsert('comments',req.body
+        ,data=>console.log('添加成功'));
+});
 
 
 app.listen(8001, () => {
