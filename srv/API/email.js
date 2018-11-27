b3fadf52d0cc05bcc9571db6bdfd432efb0f5ebb
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-28 00:56:31 +0800

    Update email.js

--- srv/API/email.js
+++ srv/API/email.js
@@ -12,14 +12,14 @@ let transporter = nodemailer.createTransport(emailConfig);
 
 
 // send mail with defined transport object
-function sendMail(emailTo,emailSubject,emailContent){
+function sendMail(to,subject,content){
     let mailOptions = {
         from: '"泛银河系含漱爆破液" <1019118008@qq.com>', // sender address
-        to: emailTo, // list of receivers
-        subject: emailSubject, // Subject line
+        to: to, // list of receivers
+        subject: subject, // Subject line
         // 发送text或者html格式
         // text: 'Hello world?', // plain text body
-        html: `<b>emailContent</b>` // html body
+        html: `<b>${content}</b>` // html body
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
