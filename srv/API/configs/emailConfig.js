0a7fe859cad3acc2dccd6f5d9ef3fdee107f74e9
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-28 21:52:27 +0800

    Update emailConfig.js

--- srv/API/configs/emailConfig.js
+++ srv/API/configs/emailConfig.js
@@ -1,11 +1,11 @@
-const emailConfig={
+const emailConfig = {
     // host: 'smtp.ethereal.email',
     service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
-        port: 465, // SMTP 端口
+    port: 465, // SMTP 端口
     secureConnection: true, // 使用了 SSL
     auth: {
-    user: '1019118008@qq.com',
+        user: '1019118008@qq.com',
         pass: 'kgytqxtfdsagbcdi',
-}
-}
-module.exports=emailConfig;
+    }
+};
+module.exports = emailConfig;
