35a125d79654b2f1049c784b46fb0927ce10738a
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-28 21:52:57 +0800

    Update Footer1.vue

--- src/components/Footer1.vue
+++ src/components/Footer1.vue
@@ -1,8 +1,7 @@
 <template>
-    <Footer>
-        <div>
+        <div><small>Copyright © 泛银河系函数爆破液 2018</small>
+            <div><small>京ICP备18046047号</small></div>
         </div>
-    </Footer>
 </template>
 
 <script>
@@ -12,9 +11,11 @@
 </script>
 
 <style scoped lang="scss">
-.ivu-layout-footer div{
-    height:60px;
-    width:100%;
-    color: #ffffff;
+div{
+    vertical-align: center;
+    text-align: center;
+    z-index: 999;
+    height:40px;
+    color: rgba(7, 60, 89, 0.44);
 }
 </style>
