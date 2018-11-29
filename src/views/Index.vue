039d5282fa87dfac10be6343b4fd03e395ef0c47
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-29 21:26:06 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,21 +1,23 @@
 <template>
-        <Layout>
-            <Header>
-                <HeadNav></HeadNav>
-            </Header>
-            <Content>
-                <Row type="flex" justify="center">
-                    <Col span="14">
-                        <AuthorBox></AuthorBox>
-                    </Col>
-                </Row>
-                <router-view></router-view>
-                <BackToTop></BackToTop>
-            </Content>
-            <Footer>
-                <Footer1></Footer1>
-            </Footer>
-        </Layout>
+    <Layout>
+        <Header>
+            <HeadNav></HeadNav>
+        </Header>
+        <Content>
+            <Row type="flex" justify="center">
+                <Col span="14">
+                    <Chat :articleid="228"></Chat>
+                    <AuthorBox></AuthorBox>
+                </Col>
+            </Row>
+            <router-view></router-view>
+            <BackToTop></BackToTop>
+
+        </Content>
+        <Footer>
+            <Footer1></Footer1>
+        </Footer>
+    </Layout>
 </template>
 
 <script>
@@ -24,35 +26,49 @@
     import HeadNav from '../components/HeadNav'
     import Footer1 from '../components/Footer1'
     import BackToTop from '../components/BackToTop'
-    // import LoadMore from '../components/LoadMore'
+    import Chat from '../components/Chat'
 
     export default {
         name: "Index",
-        components:{
+        components: {
             AuthorBox,
             HeadNav,
             Footer1,
             FeaturedAndLatest,
             BackToTop,
-
+            Chat,
         },
 
     }
 </script>
 
 <style lang="scss">
-    .ivu-card-body{
-        padding:0 !important;
+    .ivu-card-body {
+        padding: 0 !important;
     }
-    #hero {
-        /*<!--background:no-repeat 0 -600px  url(https://cdn.vuetifyjs.com/images/parallax/material2.jpg);-->*/
-    }
+
     .ivu-layout-header {
         background-color: #ddd;
         padding: 0;
     }
+
     .ivu-layout-content {
         background-color: #ddd;
+    }
+
+    ::-webkit-scrollbar {
+        width: 16px;
+        height:16px;
+        background-color: rgba(0, 205, 246, 0.05);
+    }
+
+    ::-webkit-scrollbar-track
+    {
+        background-color: #fff;
+    }
+    ::-webkit-scrollbar-thumb{
+        height:50px;
+        background: rgba(0, 190, 228, 0.13);
     }
 
 </style>
